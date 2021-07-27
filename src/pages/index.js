6c629c6ce38756f297801a16be1c1568c5eafdb5
Muhammad Bilal Raza <muhammadbilalraza@outlook.com>
2021-07-27 12:11:20 +0500

    with active hero section

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,19 +3,17 @@ import React from "react"
 // import Header from "../components/Header"
 // import Footer from "../components/Footer"
 
-// import Hero from "../components/Hero"
+import Hero from "../components/Hero"
 
 import NewHeader from "../components/NewHeader"
-
 
 export default function Home() {
   return (
     <container>
       <NewHeader></NewHeader>
       {/* <Header></Header> */}
-      {/* <Hero></Hero> */}
+      <Hero></Hero>
       {/* <Footer></Footer>   */}
     </container>
-   
   )
 }
