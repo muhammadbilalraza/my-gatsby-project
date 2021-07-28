a5cf29e814e10c82a9b68b9c2b4819a804a30293
Muhammad Bilal Raza <muhammadbilalraza@outlook.com>
2021-07-28 13:26:44 +0500

    hero section - responsiveness

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,22 +1,24 @@
-import React from 'react'
-import { Link } from 'gatsby'
+import React from "react"
+import { Link } from "gatsby"
 
 import HeroPhoto1 from "../images/hero_photo1.jpeg"
 import HeroPhoto2 from "../images/hero_photo2.jpeg"
 
-
-
 export default function Hero() {
-    return (
-        <section>
-            <div className="hero">
-                <img src={HeroPhoto1}></img>
-                <img src={HeroPhoto2}></img>
-                <p>Fine jewelry<br/>for every day</p>
-                <Link to='#shop' className='button'>
-                    <div>Shop all</div>
-                </Link>
-            </div>
-        </section>
-    )
+  return (
+    <section>
+      <div className="hero">
+        <img id="first-image" src={HeroPhoto1}></img>
+        <img id="second-image" src={HeroPhoto2}></img>
+        <p>
+          Fine jewelry
+          <br />
+          for every day
+        </p>
+        <Link to="#shop" className="button">
+          <p>Shop all</p>
+        </Link>
+      </div>
+    </section>
+  )
 }
