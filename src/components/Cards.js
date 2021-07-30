d33c53571dbae01e5f0070845505b57649596534
Muhammad Bilal Raza <muhammadbilalraza@outlook.com>
2021-07-30 19:23:56 +0500

    category card update

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -6,25 +6,29 @@ import Image3 from "../images/section2_card3.jpeg"
 
 export default function Cards() {
   return (
-    <section className="card">
-      <div className="card--content">
-        <Link to="#category" className="link--image">
-          <img src={Image1} alt="" />
-        </Link>
-        <p>Earrings</p>
+    <div className="category-section">
+      <h2 className="heading">Category</h2>
+
+      <div className="card">
+        <div className="card--content">
+          <Link to="#category" className="link--image">
+            <img src={Image1} alt="" />
+          </Link>
+          <p>Earrings</p>
+        </div>
+        <div className="card--content">
+          <Link to="#category" className="link--image">
+            <img src={Image2} alt="" />
+          </Link>
+          <p>Necklaces</p>
+        </div>
+        <div className="card--content">
+          <Link to="#category" className="link--image">
+            <img src={Image3} alt="" />
+          </Link>
+          <p>Rings</p>
+        </div>
       </div>
-      <div className="card--content">
-        <Link to="#category" className="link--image">
-          <img src={Image2} alt="" />
-        </Link>
-        <p>Necklaces</p>
-      </div>
-      <div className="card--content">
-        <Link to="#category" className="link--image">
-          <img src={Image3} alt="" />
-        </Link>
-        <p>Rings</p>
-      </div>
-    </section>
+    </div>
   )
 }
