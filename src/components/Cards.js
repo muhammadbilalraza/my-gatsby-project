e6c055557ba2bb6ee7a38d6ec0237da40d9532eb
Muhammad Bilal Raza <muhammadbilalraza@outlook.com>
2021-08-04 14:23:02 +0500

    update

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -7,27 +7,30 @@ import Image3 from "../images/section2_card3.jpeg"
 export default function Cards() {
   return (
     <div className="category-section">
-      <h2 className="heading">Category</h2>
+      <div className="category--heading">
+        <h2 className="heading">Category</h2>
+        <Link to="#shop">Shop all</Link>
+      </div>
 
       <div className="card">
-        <div className="card--content">
-          <Link to="#category" className="link--image">
-            <img src={Image1} alt="" />
-          </Link>
+        {/* <div className="card--content"> */}
+        <Link to="#category" className="link--image">
+          <img src={Image1} alt="" />
           <p>Earrings</p>
-        </div>
-        <div className="card--content">
-          <Link to="#category" className="link--image">
-            <img src={Image2} alt="" />
-          </Link>
+        </Link>
+        {/* </div> */}
+        {/* <div className="card--content"> */}
+        <Link to="#category" className="link--image">
+          <img src={Image2} alt="" />
           <p>Necklaces</p>
-        </div>
-        <div className="card--content">
-          <Link to="#category" className="link--image">
-            <img src={Image3} alt="" />
-          </Link>
+        </Link>
+        {/* </div> */}
+        {/* <div className="card--content"> */}
+        <Link to="#category" className="link--image">
+          <img src={Image3} alt="" />
           <p>Rings</p>
-        </div>
+        </Link>
+        {/* </div> */}
       </div>
     </div>
   )
