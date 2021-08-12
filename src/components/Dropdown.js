d2fdaa926d80bb512c89f7b221fd8240d8217835
Muhammad Bilal Raza <muhammadbilalraza@outlook.com>
2021-08-12 13:21:05 +0500

    dropdown hook

---
+++ src/components/Dropdown.js
@@ -0,0 +1,39 @@
+import React, { useState } from "react"
+
+export default function Dropdown({ title, items = [], multiSelect = false }) {
+  const [open, setOpen] = useState(false)
+  const [selection, setSelection] = useState([])
+  const toggle = () => setOpen(!open)
+
+  function handleOnClick(item) {}
+
+  return (
+    <div className="dd-wrapper">
+      <div
+        tabIndex={0}
+        className="dd-header"
+        role="button"
+        onKeyPress={() => toggle(!open)}
+        onClick={() => toggle(!open)}
+      >
+        <div className="dd-header_title">
+          <p className="dd-header_title--bold">{title}</p>
+        </div>
+        <div className="dd-header_action">
+          <p>{open ? "Close" : "Open"}</p>
+        </div>
+      </div>
+
+      <ul className="dd-list">
+        {items.map(item => (
+          <li className="dd-list-item" key={item.id}>
+            <button type="button" onClick={() => handleOnClick(item)}>
+              <span>{item.value}</span>
+              <span>Selected...</span>
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,6 +1,22 @@
 import React from "react"
+import Dropdown from "../components/Dropdown"
 import Layout from "../components/Layout"
 
+const items = [
+  {
+    id: 1,
+    value: "Pulp Fiction",
+  },
+  {
+    id: 2,
+    value: "The Prestige",
+  },
+  {
+    id: 3,
+    value: "Blade Runner 2049",
+  },
+]
+
 export default function about() {
-  return <Layout></Layout>
+  return <Dropdown title="Select move" items={items} />
 }
